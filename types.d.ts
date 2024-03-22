e41b39e62ab6aa21ab8325d7930d3d293f62abeb
AndMkh7 <andmkhart77@gmail.com>
2024-03-22 21:38:14 +0400

    Adding types.ts file for importing types

---
+++ types.d.ts
@@ -0,0 +1,14 @@
+type Theme = {
+  name: string;
+  id: string;
+  image: string;
+  bgColor: string;
+  tags: string[];
+};
+
+type Tag = {
+  name: string;
+  id: string;
+};
+
+export { Theme, Tag };
